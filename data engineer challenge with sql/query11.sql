5e83e14aeda7cdf602970840c302cc168524e694
Etika Widyawati <91242818+etikawdywt16@users.noreply.github.com>
2021-09-25 14:19:16 +0700

    Add files via upload

---
+++ data engineer challenge with sql/query11.sql
@@ -0,0 +1,12 @@
+#Tampilkan transaksi-transaksi yang memiliki jumlah item produk lebih dari 1 jenis produk. Dengan lain kalimat, tampilkan transaksi-transaksi yang memiliki jumlah baris data pada table tr_penjualan_detail lebih dari satu.
+
+SELECT tr_penjualan_detail.kode_transaksi,
+	ms_pelanggan.kode_pelanggan,
+	ms_pelanggan.nama_pelanggan,
+	tr_penjualan.tanggal_transaksi,
+	COUNT(tr_penjualan_detail.kode_transaksi) AS jumlah_detail
+FROM ms_pelanggan
+JOIN tr_penjualan USING(kode_pelanggan)
+JOIN tr_penjualan_detail USING(kode_transaksi)
+GROUP BY kode_transaksi
+HAVING jumlah_detail > 1;
